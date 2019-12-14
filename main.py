0b33e321b6260bb98c415902ca7009092d793c9f
Houssem BOULAHBAL <boulahbalhoussem@gmail.com>
2019-12-14 17:06:23 +0100

    verbose

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
 	from tensorflow.python.client import device_lib
 	print(device_lib.list_local_devices())
 	resizer = Image_processor()
-	data_loader = Dataset_loader([resizer],500)
+	data_loader = Dataset_loader([resizer])
 	dataset_path  =os.getcwd() + "/data/train" 
-	data, label = data_loader.load(dataset_path)
+	data, label = data_loader.load(dataset_path,500)
 	model = Model(data,label,test_size=0.25,batch_size =1)
 	model.create_model()
 	model.fit()
