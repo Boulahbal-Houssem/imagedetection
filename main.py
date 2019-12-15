738161159c58ed65b5b8391357e6d8934a2663fd
Houssem BOULAHBAL <boulahbalhoussem@gmail.com>
2019-12-15 12:11:27 +0100

    working on train dir

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from data_generator import DataGenerator, create_dataframe , split_dataframe
 import os
 
 if __name__ == "__main__":
-    data_path = os.getcwd() + "/data/test"
+    data_path = os.getcwd() + "/data/train"
     data = create_dataframe(data_path)
     trainX, valdX , testX , trainY ,valdY,testY = split_dataframe(data)    
     train_generator = DataGenerator(trainX,trainY,width=222,height=222,channel=3,batch_size=64)
-    validation_generator = DataGenerator(trainX,trainY,width=222,height=222,channel=3,batch_size=32)
+    validation_generator = DataGenerator(trainX,trainY,width=222,height=222,channel=3,batch_size=64)
     test_generator = DataGenerator(trainX,trainY,width=222,height=222,channel=3,batch_size=32)
     model = Model(train_generator,validation_generator,test_generator)
     model.create_model()
